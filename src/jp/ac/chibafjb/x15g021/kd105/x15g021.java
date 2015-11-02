455d3672c93c821d8f662a0611f958b0a89f809e
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-02 14:56:55 +0900

    105

---
+++ src/jp/ac/chibafjb/x15g021/kd105/x15g021.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g021.kd105;
+
+public class x15g021 {
+
+	public static void main(String[] args) {
+		 System.out.println("氏名：　杉山　智紀");
+    	 System.out.println("趣味は読書とアニメ鑑賞、ゲーム(カードゲームや"
+    	 		+ "ストーリー性のあるゲーム)");
+	}
+
+}
