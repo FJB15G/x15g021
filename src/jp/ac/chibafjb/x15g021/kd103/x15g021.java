ae4f070b246fc2400fb3e1830ff9b5e55388f231
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-02 14:49:32 +0900

    １０３

---
+++ src/jp/ac/chibafjb/x15g021/kd103/x15g021.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g021.kd103;
+
+public class x15g021 {
+
+	public static void main(String[] args) {
+		System.out.println("1杉山智紀　 2杉山智紀　３杉山智紀　４杉山智紀　５杉山智紀"
+        		+ "　６杉山智紀　７杉山智紀　８杉山智紀　９杉山智紀　１０杉山智紀"); 
+	}
+
+}
