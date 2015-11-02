53ef24861a9ee96b871cca7fe2f84afee9914c47
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-02 14:53:34 +0900

    104

---
+++ src/jp/ac/chibafjb/x15g021/kd104/x15g021.java
@@ -0,0 +1,8 @@
+package jp.ac.chibafjb.x15g021.kd104;
+
+public class x15g021 {
+	public static void main(String[] args){
+   	 System.out.println("あいうえお　あいうえお　あいうえお");
+   	 System.out.println("あいうえお　あいうえお　あいうえお");
+   	 System.out.println("あいうえお　あいうえお　あいうえお");
+}}
