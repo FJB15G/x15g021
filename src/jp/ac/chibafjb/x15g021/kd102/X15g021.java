90f6a1d420010eae27045398af4f6125785dc5ae
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-02 14:42:53 +0900

    杉山

---
+++ src/jp/ac/chibafjb/x15g021/kd102/X15g021.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g021.kd102;
+
+public class X15g021 {
+
+	public static void main(String[] args) {
+		System.out.println("１杉山　智紀");
+		System.out.println("２杉山　智紀");
+		System.out.println("３杉山　智紀");
+		System.out.println("４杉山　智紀");
+		System.out.println("５杉山　智紀");
+		System.out.println("６杉山　智紀");
+		System.out.println("７杉山　智紀");
+		System.out.println("８杉山　智紀");
+		System.out.println("９杉山　智紀");
+		System.out.println("１０杉山　智紀");
+
+	}
+
+}
