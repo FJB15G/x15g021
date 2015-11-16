d81bd7763b640f596160916fd19de7e95f0d7398
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-16 13:56:21 +0900

    x15g021Re101

---
+++ src/jp/ac/chibafjb/x15g021/kd101/x15g021Re101.java
@@ -0,0 +1,21 @@
+package jp.ac.chibafjb.x15g021.kd101;
+
+public class x15g021Re101 {
+		// TODO 自動生成されたメソッド・スタブ
+		/**
+		 * 課題101
+		 * @param args　
+		 * @returan なし
+		 *101 021 杉山智紀 
+		 */
+		
+		public static void main(String[] args) {
+			// 画面に文字を表示
+			//プログラムを入力
+			System.out.println("hello Java");
+			System.out.println("○○です");
+		    System.out.println("船橋情報ビジネス専門学校");
+		    System.out.println("047-425-1051（改行）Hello☆こんにちは（改行）");
+	}
+
+}
