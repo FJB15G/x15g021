c9a4bca3f68fbd1c4d94e78f437b0f3a92981ab4
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-16 15:01:37 +0900

    x15g021a115

---
+++ src/jp/ac/chibafjb/x15g021/kd115/x15g021115.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g021.kd115;
+
+public class x15g021115 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	       System.out.println("あいうえお　あいうえお　あいうえお\n"
+		        		     +"あいうえお　あいうえお　あいうえお\n"
+		        			 +"かきくけこ　かきくけこ　かきくけこ\n"
+		        			 +"かきくけこ　かきくけこ　かきくけこ\n"
+		        			 +"さしすせそ　さしすせそ　さしすせそ\n"
+		        			 +"さしすせそ　さしすせそ　さしすせそ\n");
+		}
+	}
