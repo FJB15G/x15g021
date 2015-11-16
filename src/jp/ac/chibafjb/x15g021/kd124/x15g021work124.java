9ab0bf0be293f8a1cb955a134893a2c625d45d43
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-16 15:08:01 +0900

    x15g021124

---
+++ src/jp/ac/chibafjb/x15g021/kd124/x15g021work124.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g021.kd124;
+
+public class x15g021work124 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i;
+		for(i=0;i<10;i++) 
+			System.out.print("☆");
+	}
+
+}
