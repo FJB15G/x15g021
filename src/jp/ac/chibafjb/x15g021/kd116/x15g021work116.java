34f81a5166afb86c37146731c5a1549636da39eb
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-16 14:41:53 +0900

    x15g021work115and116

---
+++ src/jp/ac/chibafjb/x15g021/kd116/x15g021work116.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g021.kd116;
+
+public class x15g021work116 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i ;
+		for (i=0;i<2;i=i+1)
+			System.out.println("あいうえお　あいうえお　あいうえお\n"
+				              +"かきくけこ　かきくけこ　かきくけこ\n"
+				              +"さしすせそ　さしすせそ　さしすせそ\n"); 
+	}
+}
