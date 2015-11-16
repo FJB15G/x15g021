0afc644caba85b722e2f3dfdbf05f9fddd6aecde
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-16 14:10:30 +0900

    x15g021Re114

--- src/jp/ac/chibafjb/x15g021/kd114/x15g021.java
+++ src/jp/ac/chibafjb/x15g021/kd114/x15g021.java
@@ -2,14 +2,13 @@ package jp.ac.chibafjb.x15g021.kd114;
 
 public class x15g021 {
 	  public static void main(String[] args){
-	int i ;
-	for(i=0;i<2;i=i+1)  
-        System.out.println("あいうえお　あいうえお　あいうえお");
-    int i1;
-    for(i1=0;i1<2;i1=i1+1)  
-        System.out.println("かきくけこ　かきくけこ　かきくけこ");
-    int i11;
-    for(i11=0;i11<2;i11=i11+1)  
-        System.out.println("さしすせそ　さしすせそ　さしすせそ");
-    }
+			/**課題114
+		     * @param    args　プログラム引数    
+		     * @return     なし                 
+		     * 1G1 21 杉山智紀             
+		    */
+		      
+		    int i;for(i=0;i<5;i=i+1)  
+		        System.out.println("***");
+	}
 }
