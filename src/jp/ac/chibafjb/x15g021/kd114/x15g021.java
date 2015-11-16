ffa16d1dd5a657af213774272ec7c1cb5c3d7aa6
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-16 14:03:30 +0900

    x15g021work114

--- src/jp/ac/chibafjb/x15g021/kd114/x15g021.java
+++ src/jp/ac/chibafjb/x15g021/kd114/x15g021.java
@@ -1,10 +1,15 @@
 package jp.ac.chibafjb.x15g021.kd114;
 
 public class x15g021 {
-	int i;for(i=0;i<2;i=i+1)  
+	  public static void main(String[] args){
+	int i ;
+	for(i=0;i<2;i=i+1)  
         System.out.println("あいうえお　あいうえお　あいうえお");
-    int i1;for(i1=0;i1<2;i1=i1+1)  
+    int i1;
+    for(i1=0;i1<2;i1=i1+1)  
         System.out.println("かきくけこ　かきくけこ　かきくけこ");
-    int i11;for(i11=0;i11<2;i11=i11+1)  
+    int i11;
+    for(i11=0;i11<2;i11=i11+1)  
         System.out.println("さしすせそ　さしすせそ　さしすせそ");
-｝
+    }
+}
