0ceb6322a23700704cdb0689c3ba78068fc26560
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2015-11-16 14:13:20 +0900

    x15g021work115

---
+++ src/jp/ac/chibafjb/x15g021/kd115/x15g021115.java
@@ -0,0 +1,19 @@
+package jp.ac.chibafjb.x15g021.kd115;
+
+public class x15g021115 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i ;
+		for(i=0;i<2;i=i+1)  
+	        System.out.println("あいうえお　あいうえお　あいうえお");
+	    int i1;
+	    for(i1=0;i1<2;i1=i1+1)  
+	        System.out.println("かきくけこ　かきくけこ　かきくけこ");
+	    int i11;
+	    for(i11=0;i11<2;i11=i11+1)  
+	        System.out.println("さしすせそ　さしすせそ　さしすせそ");
+	    
+	}
+
+}
