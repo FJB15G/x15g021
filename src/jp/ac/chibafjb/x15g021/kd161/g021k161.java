98f82435331426f075b96d64f40d69d5006540d9
unknown <x15g021@PC504N022.chiba-fjb.ac.jp>
2016-02-01 15:40:24 +0900

    15g021

--- src/jp/ac/chibafjb/x15g021/kd161/g021k161.java
+++ src/jp/ac/chibafjb/x15g021/kd161/g021k161.java
@@ -1,21 +1,17 @@
 package jp.ac.chibafjb.x15g021.kd161;
 
 public class g021k161 {
-
-	public static void main(String[] args) {
-//		int[] test= 0,1;
-//		test = new int[3];
-//		test[0] = 20;
-//		test[1] = 20;
-//
-//		System.out.println(test[0]);
-//		System.out.println(test[1]);
-//	}
-	int[] a = {5,8,2,1,4};
+		
+public static void main(String[] args) {
+	int[] test = {10,20};
+	int work = test[1];
+	test[1] = test[0];
+	test[0] = work;
 	int i;
-	for(i=0;i<a.length;i++)
+	for(i=0;i<test.length;i++)
 		{
-	    System.out.println([i]);
+	    System.out.println("test["+i+"]="+test[i]);
 		}
+	
 	}
 }	
